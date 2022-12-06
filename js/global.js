002599f0b8299d1da7f8a95b612bcc0c996d496c
Emmalandgren <emma.landgren04@skola.stenungsund.se>
2022-12-06 13:50:33 +0100

    6/12-22

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.abstract = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
